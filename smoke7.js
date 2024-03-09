927cf45505dc6bea92b6a5ae995c16222c825f1f
marko8989 <162463167+marko8989@users.noreply.github.com>
2024-03-09 23:07:35 +0100

    Create smoke8.js

--- smoke7.js
+++ smoke7.js
@@ -606,8 +606,8 @@ function multipleSplats(amount) {
     const color = [0.529, 0.357, 1.0];
     const x = canvas.width * Math.random();
     const y = canvas.height * Math.random();
-    const dx = 1000 * (Math.random() - 0.5);
-    const dy = 1000 * (Math.random() - 0.5);
+    const dx = 300 * (Math.random() - 0.5);
+    const dy = 300 * (Math.random() - 0.5);
     splat(x, y, dx, dy, color);
   }
 }
